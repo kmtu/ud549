cbbac5235f01c032559d7c802858f58287d8f1ff
KM Tu <me@kmtu.info>
2017-04-06 13:39:58 +0900

    Async done

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,5 +1,6 @@
 function AddressBook() {
   this.contacts = [];
+  this.initialComplete = false;
 }
 
 AddressBook.prototype.addContact = function(contact) {
@@ -13,4 +14,15 @@ AddressBook.prototype.getContact = function(idx) {
 AddressBook.prototype.deleteContact = function(idx) {
   this.contacts.splice(idx, 1);
   console.log("log: " + this.contacts[0]);
+}
+
+AddressBook.prototype.getInitialContacts = function(cb) {
+  var self = this;
+
+  setTimeout(function() {
+    self.initialComplete = true;
+    if (cb) {
+      return cb();
+    }
+  }, 3);
 }
